f6147e33d72fbce60520cec3b4c7ac540b68c8e8
DannySamy3 <danielntunduye@gmail.com>
2025-06-02 16:48:23 +0300

    fixed cors

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://church-app-dev.netlify.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
